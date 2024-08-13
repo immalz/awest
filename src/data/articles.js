75abacc697348a798981095cbc02935907ce0fdc
Marco Alzamora <alzamora1008@gmail.com>
2024-08-12 20:35:40 -0500

    viewtransition en cards de eventos

---
+++ src/data/articles.js
@@ -0,0 +1,40 @@
+export const articles = [
+    {
+        id: 1,
+        url: 'sorteo-antologia-bocchi-the-rock',
+        title: 'Article 1',
+        content: 'Content of article 1',
+        banner: 'sorteo-bocchi.png',
+        gifts: [
+            {
+                title: 'Gift 1',
+                img: 'gift1.jpg',
+                alt: 'Gift 1',
+            },
+            {
+                title: 'Gift 2',
+                img: 'gift2.jpg',
+                alt: 'Gift 2',
+            }
+        ]
+    },
+    {
+        id: 2,
+        url: 'sorteo-overlord',
+        title: 'Article 2',
+        content: 'Content of article 2',
+        banner: 'sorteo-overlord.png',
+        gifts: [
+            {
+                title: 'Gift 1',
+                img: 'gift1.jpg',
+                alt: 'Gift 1',
+            },
+            {
+                title: 'Gift 2',
+                img: 'gift2.jpg',
+                alt: 'Gift 2',
+            }
+        ]
+    }
+]
